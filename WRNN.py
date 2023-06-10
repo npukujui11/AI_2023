c3038b461e2aa62077f81d8d3f1e4a39140edd9b
Ku Jui <npukujui11@gmail.com>
2023-06-11 03:18:38 +0800

    Update WRNN.py
    调整Model Initializing的位置

--- WRNN.py
+++ WRNN.py
@@ -30,46 +30,6 @@ ix_to_char = {i: ch for i, ch in enumerate(chars)}  # 将 chars 中的索引 ind
 
 ###############
 ###############
-# Model Initializing #
-###############
-###############
-
-# 模型超参数（要修改的话，请修改这里 by Gu Rui）
-hidden_size = 160  # Hidden layer size
-seq_length = 30  # RNN sequence length
-learning_rate = 1e-2  # Learning rate
-
-"""
-### 定义超参数
-隐藏层是循环神经网络（RNN）中的一部分，决定了网络的容量和表示能力。
-较大的隐藏层可以捕捉更复杂的模式，但也会增加计算开销和过拟合的风险。在实际应用中，隐藏层的大小一般在 50 到 1000 之间。
-
-序列长度是循环神经网络（RNN）中的另一个重要参数。序列长度决定了每次训练和预测时网络能够看到多少个字符。
-序列长度越长，网络就能够看到更多的上下文信息，但也会增加计算开销和训练时间。在实际应用中，序列长度一般在 25 到 100 之间。
-
-学习率是控制网络权重更新速度的参数，决定了网络的学习速度。较大的学习率可以加快网络的学习速度，但也会增加训练过程中的不稳定性。
-在实际应用中，学习率一般在 1e-2 到 1e-5 之间。
-"""
-
-# 初始化权重矩阵和bias
-Ux = np.random.randn(hidden_size, vocab_size) * 0.01  # 输入到隐藏层a的权重
-Wx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层a到隐藏层a的权重
-Vx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层a到隐藏层b的权重
-Rx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层a到隐藏层b的权重
-Tx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层b到隐藏层b的权重
-Qx = np.random.randn(vocab_size, hidden_size) * 0.01  # 隐藏层b到输出的权重
-
-s1 = np.zeros((hidden_size, 1))  # 隐藏层a的偏置
-s2 = np.zeros((hidden_size, 1))  # 隐藏层b的偏置
-s3 = np.zeros((vocab_size, 1))  # 输出层的偏置
-
-"""
--------------------------------------------------------------------------------
-"""
-
-
-###############
-###############
 # Loss Function #
 ###############
 ###############
@@ -179,6 +139,45 @@ def sample(hprev, seed_ix, n):
         ixes.append(ix)  # 将抽取的整数添加到整数序列中
     return ixes  # 返回整数序列
 
+
+"""
+-------------------------------------------------------------------------------
+"""
+
+###############
+###############
+# Model Initializing #
+###############
+###############
+
+# 模型超参数（要修改的话，请修改这里 by Gu Rui）
+hidden_size = range(50, 1001, 50)  # Hidden layer size
+seq_length = range(25, 101, 10)  # RNN sequence length
+learning_rate = [1e-2, 1e-3, 1e-4, 1e-5]  # Learning rate
+
+"""
+### 定义超参数
+隐藏层是循环神经网络（RNN）中的一部分，决定了网络的容量和表示能力。
+较大的隐藏层可以捕捉更复杂的模式，但也会增加计算开销和过拟合的风险。在实际应用中，隐藏层的大小一般在 50 到 1000 之间。
+
+序列长度是循环神经网络（RNN）中的另一个重要参数。序列长度决定了每次训练和预测时网络能够看到多少个字符。
+序列长度越长，网络就能够看到更多的上下文信息，但也会增加计算开销和训练时间。在实际应用中，序列长度一般在 25 到 100 之间。
+
+学习率是控制网络权重更新速度的参数，决定了网络的学习速度。较大的学习率可以加快网络的学习速度，但也会增加训练过程中的不稳定性。
+在实际应用中，学习率一般在 1e-2 到 1e-5 之间。
+"""
+
+# 初始化权重矩阵和bias
+Ux = np.random.randn(hidden_size, vocab_size) * 0.01  # 输入到隐藏层a的权重
+Wx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层a到隐藏层a的权重
+Vx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层a到隐藏层b的权重
+Rx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层a到隐藏层b的权重
+Tx = np.random.randn(hidden_size, hidden_size) * 0.01  # 隐藏层b到隐藏层b的权重
+Qx = np.random.randn(vocab_size, hidden_size) * 0.01  # 隐藏层b到输出的权重
+
+s1 = np.zeros((hidden_size, 1))  # 隐藏层a的偏置
+s2 = np.zeros((hidden_size, 1))  # 隐藏层b的偏置
+s3 = np.zeros((vocab_size, 1))  # 输出层的偏置
 
 """
 -------------------------------------------------------------------------------
