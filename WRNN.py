5c72385254c42ea50241248ecf50b19222c81ea2
Ku Jui <npukujui11@gmail.com>
2023-06-10 21:42:30 +0800

    Update WRNN.py
    实现定义lossFun函数，实现WRNN参数的初始化、前向传播、损失计算、反向传播等功能。

--- WRNN.py
+++ WRNN.py
@@ -82,6 +82,67 @@ def softmax(x):
     e_x = np.exp(x - np.max(x)) # 防止溢出
     return e_x / e_x.sum(axis=0) # softmax函数
 
+def lossFun(inputs, targets):
+  """
+  input: inputs,targets为整数列，对应模型的输入与输出结果.
+  output: return 损失、模型参数的梯度。
+  by Gu Rui
+
+  这里实现一个WRNN（一种改进的RNN模型）的损失函数，该模型包含两个隐藏层，分别为a和b，其中a层为RNN，b层为LSTM。
+  其结构见附图W-RNN作业.pdf, 其中：x为输入，a为RNN隐藏层，b为LSTM隐藏层，o为输出层，Ux、Wx、Vx、Rx、Tx、Qx为权重矩阵，s1、s2、s3为偏置。
+  f1和f2为激活函数，分别为sigmoid和sigmoid（此处未定义f1和f2）f3为softmax激活函数。
+
+  前向传播公式有（latex）：$a^t=f_1(Ux^t+Wa^{t-1}+s_1)$、$b^t=f_2(Vx^t+Ra^{t-1}+Tb^{t-1}+s_2)$、$o^t=f_3(Qx^t+Tb^t+s_3)$
+
+  该函数实现了WRNN参数的初始化、前向传播、损失计算、反向传播。
+  """
+  x, a, b, o = {}, {}, {}, {} # 初始化输入、隐藏层a、隐藏层b、输出层的字典
+  a[-1] = np.zeros((hidden_size, 1)) # 初始化隐藏层a的初始状态
+  b[-1] = np.zeros((hidden_size, 1)) # 初始化隐藏层b的初始状态
+  loss = 0 # 初始化损失
+
+  # 前向传播过程
+  for t in range(len(inputs)):  # 对序列中的每个字符
+      x = np.zeros((vocab_size, 1)) # 初始化输入
+      x[inputs[t]] = 1 # 将当前字符对应的输入置为1
+      a[t] = sigmoid(np.dot(Ux, x) + np.dot(Wx, a[t - 1]) + s1) # 计算隐藏层a
+      b[t] = sigmoid(np.dot(Vx, a[t]) + np.dot(Rx, a[t - 1]) + np.dot(Tx, b[t - 1]) + s2) # 计算隐藏层b
+      o[t] = softmax(np.dot(Qx, b[t]) + s3) # 计算输出层
+      loss += -np.log(o[t][targets[t], 0]) # softmax (cross-entropy loss) 计算损失
+
+  # 后向传播过程
+  dUx, dWx, dVx, dRx, dTx, dQx = np.zeros_like(Ux), np.zeros_like(Wx), np.zeros_like(Vx), \
+      np.zeros_like(Rx), np.zeros_like(Tx), np.zeros_like(Qx) # 初始化参数梯度
+
+  ds1, ds2, ds3 = np.zeros_like(s1), np.zeros_like(s2), np.zeros_like(s3) # 初始化偏置梯度
+
+  for t in reversed(range(len(inputs))): # 对序列中的每个字符
+    do = o[t].copy()
+    do[targets[t]] -= 1 # backprop into o
+
+    dQx += np.dot(do, b[t].T) # backprop into Qx
+    ds3 += do # backprop into s3
+
+    dbt = np.dot(Qx.T, do) # backprop into b
+    dbt_raw = dbt * (1 - b[t] * b[t]) # backprop through tanh nonlinearity
+
+    dVx += np.dot(dbt_raw, a[t].T) # backprop into Vx
+    dRx += np.dot(dbt_raw, a[t - 1].T) # backprop into Rx
+    dTx += np.dot(dbt_raw, b[t - 1].T) # backprop into Tx
+    ds2 += dbt_raw # backprop into s2
+
+    dat = np.dot(Vx.T, dbt_raw) + np.dot(Rx.T, dbt_raw) # backprop into a
+    dat_raw = dat * (1 - a[t] * a[t]) # backprop through tanh nonlinearity
+
+    dUx += np.dot(dat_raw, x.T) # backprop into Ux
+    dWx += np.dot(dat_raw, a[t - 1].T) # backprop into Wx
+    ds1 += dat_raw # backprop into s1
+
+  for dparam in [dUx, dWx, dVx, dRx, dTx, dQx, ds1, ds2, ds3]: # clip to mitigate exploding gradients
+    np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
+
+  return loss, dUx, dWx, dVx, dRx, dTx, dQx, ds1, ds2, ds3 # 返回损失和参数梯度
+
 """
 -------------------------------------------------------------------------------
 """
