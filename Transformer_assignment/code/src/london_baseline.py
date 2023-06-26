b857ec96e503fe1b40151467c16bbdd2f2ecdb34
Ku Jui <npukujui11@gmail.com>
2023-06-27 00:26:46 +0800

    Update london_baseline.py

--- Transformer_assignment/code/src/london_baseline.py
+++ Transformer_assignment/code/src/london_baseline.py
@@ -2,3 +2,12 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+import utils
+
+eval_corpus_path = "D:\\program\\AI_2023\\Transformer_assignment\\code\\birth_dev.tsv"
+n = sum(1 for _ in open(eval_corpus_path,encoding="utf-8"))
+predictions = ["London"]*n
+
+total, correct = utils.evaluate_places(eval_corpus_path, predictions)
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
